02de107c995139ec984f80882033bc47c783b4d1
Itakashiraiya <79550547+itakashiraiya@users.noreply.github.com>
2024-12-18 14:39:47 +0000

    i/o update

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/big"
+
 	// "math/rand"
 	"os"
-	"time"
 
 	"github.com/itakashiraiya/uuids/internals/uuids"
 	"golang.org/x/term"
@@ -35,19 +36,79 @@ func start() {
 	loop()
 }
 
+type inputCmd string
+
 func loop() {
 	// rand := rand.New(rand.NewSource(4))
 	pos := big.NewInt(0)
-	for i := range 1 {
+	usrString := ""
+	internal(usrString, pos)
+	for {
 		// pos := new(big.Int).Rand(rand, new(big.Int).Add(uuids.MaxEntropyNum(), big.NewInt(1)))
-		internal(new(big.Int).Add(pos, big.NewInt(int64(i))))
-		time.Sleep(time.Second * 1)
+		input, err := input()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		if val, ok := input.(inputCmd); ok {
+			switch val {
+			case "A":
+				if pos.Cmp(big.NewInt(0)) > 0 {
+					pos.Add(pos, big.NewInt(-1))
+				}
+			case "B":
+				if pos.Cmp(uuids.MaxEntropyNum()) < 0 {
+					pos.Add(pos, big.NewInt(1))
+				}
+			case "exit":
+				return
+			}
+		} else if val, ok := input.(byte); ok {
+			usrString += string(val)
+		}
+		internal(usrString, pos)
 	}
 }
 
-func internal(pos *big.Int) {
+func input() (interface{}, error) {
+	var input [3]byte
+	n, err := os.Stdin.Read(input[:])
+	if err != nil {
+		panic(err)
+	}
+	if n == 1 {
+		return input[0], nil
+	}
+
+	if n == 2 && input[0] == 27 && input[1] == 27 {
+		return inputCmd("exit"), nil
+	}
+
+	if n == 3 && input[0] == 27 {
+		if input[1] == 91 {
+			switch input[2] {
+			case 'A', 'B':
+				return inputCmd(input[2]), nil
+			}
+		}
+	}
+
+	return nil, errors.New(fmt.Sprintf("unknown input (n = %d, bytes = %d\r", n, input))
+}
+
+func inputOld() (escSeq bool, input [3]byte, err error) {
+	n, err := os.Stdin.Read(input[:])
+
+	if n == 3 && input[0] == 27 && input[1] == 91 {
+		return true, input, nil
+	}
+	return false, input, nil
+}
+
+func internal(usrString string, pos *big.Int) {
+	display := []string{usrString}
 	uuids := uuids.GetUuids(pos, display_height)
-	render(uuids)
+	display = append(display, uuids...)
+	render(display)
 }
 
 func render(outputs []string) {
